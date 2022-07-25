ad595772dbd73c8d3bcf726084daf684bbbb99ba
Kenan995 <kenan.kuloglija@fet.ba>
2022-07-25 11:04:51 +0200

    Added to more info to forecast and about page

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) =>{
         } else if (body.error) {
             callback('Unable to find coordinates. Try another search.', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently '+ body.current.temperature + ' degrees out. It feels like its ' + body.current.feelslike + ' degrees out!')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently '+ body.current.temperature + ' degrees out. It feels like its ' + body.current.feelslike + ' degrees out! The windspeed is ' + body.current.wind_speed + ', and the wind dirrection is ' + body.current.wind_dir + '.')
         }
     })
 }
